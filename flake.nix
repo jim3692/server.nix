348dbed1a62230b74e2605f847ef2f68a95bf91d
jim3692 <jim3692@gmail.com>
2024-07-20 13:00:34 +0300

    Update to NixOS 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake providing a framework for steamlined declarative management of NixOS containers and VMs";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     microvm = {
       url = "github:jim3692/microvm.nix/console-in-unix-sock";
       inputs.nixpkgs.follows = "nixpkgs";
